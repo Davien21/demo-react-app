cf32f7ca6956164b479602fb160c055f563da2f0
davien21 <chidiebereekennia@gmail.com>
2020-11-03 01:06:31 +0100

    learning spread with objects

--- src/index.js
+++ src/index.js
@@ -4,3 +4,14 @@ const second = [4, 5, 6]
 // spread operator
 const combined = [...first, ...second]
 const clone = [...first]
+
+
+const firstO = { name: 'Mosh' };
+const secondO = { job: 'Instructor' };
+
+const combinedObject = {
+  ...firstO, ...secondO, 
+  location: 'Australia'
+}
+
+const cloneO = { ...first };
